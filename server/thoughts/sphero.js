05e5b90c2ad8ee54a6ac659dd5b9a2617d372c7c
jgretz <jgretz@truefitsolutions.com>
2015-11-25 20:37:02 -0500

    add basic for sphero

--- server/thoughts/sphero.js
+++ server/thoughts/sphero.js
@@ -1,10 +1,28 @@
-var Thought = require('../thought.js');
+var port = "/dev/tty.Sphero-YOR-AMP-SPP";
+var Sphero = require('../sphero/sphero.js');
 
-class Sphero extends Thought {
+var Thought = require('../thought.js');
+class SpheroControl extends Thought {
 	id() {
 		return "Sphero";
 	}
 
+	status() {
+		var sphero = Sphero();
+		
+		sphero.on('open', function() {
+			console.log("opened");
+			sphero.setRGB(0x0000ff, false);
+		});
+
+		sphero.on('error', function(error) {
+		  console.log(error);
+		});
+
+		console.log("Trying to open");
+		sphero.open(port);
+	}
+
 }
 
-module.exports = new Sphero();
+module.exports = new SpheroControl();
